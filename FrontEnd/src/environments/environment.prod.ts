b50a071016e98cdd02fa24b11f0fe9133d404668
Dipankar Sutradhar <dsutradhar@slb.com>
2021-05-16 18:01:05 +0530

    updated  deployment settings

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlAddress: 'https://covid19directory.azurewebsites.net'
+  urlAddress: 'https://covid19-directory-backend.azurewebsites.net'
 };
